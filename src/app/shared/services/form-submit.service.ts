5d8c4c99bd5ceb9d61de1ced66fea80f1e4e31f0
Iago Alves <juliaraquel561@gmail.com>
2026-02-08 17:07:00 -0300

    ajustado form submit compatibilidade

--- src/app/shared/services/form-submit.service.ts
+++ src/app/shared/services/form-submit.service.ts
@@ -1,22 +1,9 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+/**
+ * @deprecated Use `EmailService` from `src/app/shared/email/email.service.ts`.
+ * This file remains only to avoid breaking imports if someone referenced the old path.
+ */
+export {
+  EmailService as FormSubmitService
+} from '../email/email.service';
 
-export interface FormSubmitPayload {
-  [key: string]: string;
-}
-
-@Injectable({ providedIn: 'root' })
-export class FormSubmitService {
-  private readonly endpoint = 'https://formsubmit.co/ajax/ficusrf2026@gmail.com';
-  private readonly headers = new HttpHeaders({
-    'Content-Type': 'application/json',
-    Accept: 'application/json'
-  });
-
-  constructor(private readonly http: HttpClient) {}
-
-  send(payload: FormSubmitPayload): Observable<unknown> {
-    return this.http.post(this.endpoint, payload, { headers: this.headers });
-  }
-}
+export type { EmailPayload as FormSubmitPayload } from '../email/email.service';
